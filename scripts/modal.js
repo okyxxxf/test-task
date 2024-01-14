f23e0839ef1e43baa0ba96eacb6f668d1c58202a
Alexander Bachinskiy <okyxxxf@gmail.com>
2024-01-14 11:41:14 +0300

    fix: add overflow hidden while open modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,15 +4,18 @@ window.addEventListener('load', () => {
 
   filtersIcon.addEventListener('click', () => {
     modal.classList.add('active');
+    document.body.style.overflowY = "hidden";
 
     const cross = document.querySelector('.cross-icon');
     cross.addEventListener('click', () => {
       modal.classList.remove('active');
+      document.body.style.overflowY = "scroll";
     })
 
     const submitButton = document.querySelector('.modal__button');
     submitButton.addEventListener('click', () => {
       modal.classList.remove('active');
+      document.body.style.overflowY = "scroll";
     })
   })
 });
